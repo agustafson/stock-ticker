418e774510f48eb18fd96b036903b66aa2c6d379
agustafson <andrew.gustafson@itv.com>
2017-01-27 23:02:35 +0000

    Add StockValuationService.dailyReturns

--- src/main/scala/ticker/service/StockValuationService.scala
+++ src/main/scala/ticker/service/StockValuationService.scala
@@ -9,4 +9,12 @@ import scalaz.concurrent.Task
 class StockValuationService(stockTickerService: StockTickerService) {
   def dailyPrices(businessDate: LocalDate, ticker: TickSymbol): Task[List[BigDecimal]] =
     stockTickerService.dailyPrices(businessDate, ticker).map(_.map(_.close))
+
+  def dailyReturns(businessDate: LocalDate, ticker: TickSymbol): Task[List[BigDecimal]] =
+    stockTickerService.dailyPrices(businessDate, ticker).map {
+      _.sliding(2).map {
+        case List(tickCurrent, tickPrevious) =>
+          (tickCurrent.close - tickPrevious.close) / tickPrevious.close
+      }.toList
+    }
 }
--- src/test/scala/ticker/service/StockValuationServiceTest.scala
+++ src/test/scala/ticker/service/StockValuationServiceTest.scala
@@ -29,6 +29,37 @@ class StockValuationServiceTest extends FunSpec with Matchers with MockFactory w
         results.head should ===(BigDecimal("832.150024"))
         results.last should ===(BigDecimal("697.77002"))
       }
+
+      it("from a valid date for an unknown tick symbol") {
+        val tickSymbol   = TickSymbol("ZZZZ")
+        val businessDate = LocalDate.of(2017, 1, 27)
+        (stockTickerService.dailyPrices _).when(businessDate, tickSymbol).returns(Task.now(List.empty))
+
+        val results = stockValuationService.dailyPrices(businessDate, tickSymbol).unsafePerformSync
+        results shouldBe 'empty
+      }
+    }
+
+    describe("should calculate daily returns") {
+      it("from a valid date for a known tick symbol") {
+        val tickSymbol   = TickSymbol("GOOG")
+        val businessDate = LocalDate.of(2017, 1, 27)
+        (stockTickerService.dailyPrices _).when(businessDate, tickSymbol).returns(Task.now(testDataSet))
+
+        val results = stockValuationService.dailyReturns(businessDate, tickSymbol).unsafePerformSync
+        results should have size 230
+        results.head should ===(BigDecimal("-0.00421214004") +- 0.00000000001)
+        results.last should ===(BigDecimal("0.03015316995") +- 0.00000000001)
+      }
+
+      it("from a valid date for an unknown tick symbol") {
+        val tickSymbol   = TickSymbol("ZZZZ")
+        val businessDate = LocalDate.of(2017, 1, 27)
+        (stockTickerService.dailyPrices _).when(businessDate, tickSymbol).returns(Task.now(List.empty))
+
+        val results = stockValuationService.dailyReturns(businessDate, tickSymbol).unsafePerformSync
+        results shouldBe 'empty
+      }
     }
   }
 
